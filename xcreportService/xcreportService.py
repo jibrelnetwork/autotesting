cddc183f1230a5aaf0a5a8bf379c902787a4bc3b
Bogdan Baglai <bogdan.baglai@jibrel.network>
2019-02-21 13:01:58 +0300

    add @app.route('/1_Test/Attachments/<screenshotPath>')

--- xcreportService/xcreportService.py
+++ xcreportService/xcreportService.py
@@ -6,6 +6,7 @@ from flask import request
 from flask import Response
 from pathlib import Path
 import subprocess
+import glob
 
 
 app = Flask(__name__)
@@ -20,14 +21,34 @@ def resContent(xcresult_path):
         if not Path(index_path).exists():
             command = 'xchtmlreport -r "{}"'.format(xcresult_path)
             print("Report generation: {}".format(command))
-            res = subprocess.Popen(['xchtmlreport', '-r', '"{}"'.format(xcresult_path)], stdout=subprocess.PIPE).communicate()[0]
+            res = subprocess.Popen(['/usr/local/bin/xchtmlreport', '-r', '{}'.format(xcresult_path)], stdout=subprocess.PIPE).communicate()[0]
             print(res)
             print("Report generation done")
-        return Path(index_path).read_text()
+            if Path(index_path).exists():
+                return Path(index_path).read_text()
+            else:
+                return "Report generation fail: {}".format(res)
+        else:
+            return Path(index_path).read_text()
     else:
-        return "no xcresult"
+        return "no xcresult on: {}".format(xcresult_path)
 
+@app.route('/1_Test/Attachments/<screenshotPath>')
+def screenshot(screenshotPath):
+    #http://0.0.0.0:5244/1_Test/Attachments/Screenshot%20of%20main%20screen%20(ID%201)_1_5BE71AF9-B68E-4A3A-9196-8C20E5AAA0EF.png
+    #http://192.168.7.57:5244/1_Test/Attachments/JWI-T84_1_66AD4AAE-E08B-4E8D-8835-67CA6C4A0764.png
+    # screenshotPathPart = "{}_{}*".format(screenshotPath.split("_")[0], screenshotPath.split("_")[1])
+    # /Library/Developer/XcodeServer/IntegrationAssets/1d3cf83551620b4254fde68e36cbd7ef-Simulator iPhone 6 10.3.1/30/xcodebuild_result.xcresult/1_Test/Attachments
+    findPath = '/Library/Developer/XcodeServer/IntegrationAssets/**/1_Test/Attachments/{}*'.format(screenshotPath)
+    # findPath = '/Users/bogdan/xcode/jibral/testReports/**/1_Test/Attachments/{}'.format(screenshotPath)
+    for filename in glob.iglob(findPath, recursive=True):
+        filename
+        break
 
+    if Path(filename).exists():
+        return Path(filename).read_bytes()
+    else:
+        return "No screenshot"
 
 @app.route('/')
 def index():
@@ -46,4 +67,4 @@ def get_status():
     return resp
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5244, debug=False)
+    app.run(host='0.0.0.0', port=5244, debug=True)
